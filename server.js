6b85cd6ecf98578711a8e1f36a796deedd131bde
sephoyamd-tech <sephoyamd@gmail.com>
2026-03-18 18:21:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mysql = require('mysql2');
 const cors = require('cors');
 const app = express();
+app.use(express.static(__dirname));
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
